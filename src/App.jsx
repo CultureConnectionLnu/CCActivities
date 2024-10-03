f8d20b88ec9ac9274e9ddc2b326feb6405cc9dab
Arturyux <artur.burlakin@gmail.com>
2024-10-03 14:02:13 +0200

    Get Start pack, logo, background, viteReact jsx,awesome font

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import CCLogo from "./assets/CCLogo.png";
+import SocialIcons from "./Socialmedia.jsx";
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="flex justify-center items-center min-h-screen">
+      <div className="max-w-xl mx-auto p-6">
+        <div className="h-70 w-70 mx-auto sm:w-96 sm:h-96">
+          <div className="aspect-square">
+            <img
+              src={CCLogo}
+              className="rounded-full object-cover object-center cursor-pointer"
+              alt="Logo"
+            />
+          </div>
+        </div>
+        <SocialIcons />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
